d14b8cb3a3182abc7f06b803bf7d1fc8d340cc97
Gladiu <maurycyk1@tlen.pl>
2021-11-03 02:02:43 +0000

    Skończone TS

--- TS/2/1.py
+++ TS/2/1.py
@@ -1,25 +1,58 @@
 import numpy as np 
-import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
-import math
+import matplotlib.pyplot as plot
+import scipy.signal as sig
 
-R1 = .2
-L = 0.1
-C = 0.05
-def force(t):
-    A = 2
-    return 1 * A 
+tf1 =  sig.TransferFunction([10.0], [1.0, 2.0])
+tf2 =  sig.TransferFunction([4.0], [2.0, 0, 1.0])
+tf3 =  sig.TransferFunction([-2.0, 6.0], [1.0, 7.0, 16.0, 12.0])
 
-def obj(t, x):
-    # x1 = i1
-    # x2 = q2
-    dx0 = -(R1/L)*x[0]-(1.0/L*C)*x[1]+(1.0/L)*force(t)
-    dx1 = x[0]-(0.25*(1.0/C)*x[1])/(5.0-(1.0/C)*x[1])
-    return [dx0, dx1]
+ss1 = sig.StateSpace(np.array([-2.0]), np.array([10.0]), 1, 0)
+ss2 = sig.StateSpace(np.array([[0.0, 1.0], [-0.5, 0.0]]), np.array([[0],[2.0]]), np.array([0.0, 1.0]), 0)
+ss3 = sig.StateSpace(np.array([[0, 1.0, 0], [0, 0, 1.0], [-12.0, -16.0, 7.0]]), np.array([[0], [-2.0], [20.0]]), np.array([1.0, 0, 0]), 0)
 
-res = solve_ivp(obj, [0,2], [0,0], rtol=1e-10)
+ss11 = sig.tf2ss(tf1.num, tf1.den)
+ss21 = sig.tf2ss(tf2.num, tf2.den)
+ss31 = sig.tf2ss(tf3.num, tf3.den)
 
-plt.plot(res.t, res.y[0])
-plt.plot(res.t, res.y[1])
+t = np.linspace(0, 20, 100)
+u = np.sin(t)
 
-plt.show()
+plot.figure()
+plot.title("System pierwszy")
+res = sig.step(tf1)
+res = sig.lsim(tf1, u, t)
+plot.plot(res[0], res[1])
+res = sig.step(ss1)
+res = sig.lsim(ss1, u, t)
+plot.plot(res[0], res[1])
+res = sig.step(ss11)
+res = sig.lsim(ss11, u, t)
+plot.plot(res[0], res[1])
+
+plot.figure()
+plot.title("System drugi")
+res = sig.step(tf2)
+res = sig.lsim(tf2, u, t, np.array([[0,10]]))
+plot.plot(res[0], res[1])
+res = sig.step(ss2)
+res = sig.lsim(ss2, u, t, np.array([[0,10]]))
+plot.plot(res[0], res[1])
+res = sig.step(ss21)
+res = sig.lsim(ss21, u, t, np.array([[0.0, 10.0]]))
+plot.plot(res[0], res[1])
+
+plot.figure()
+plot.title("System trzeci")
+res = sig.step(tf3)
+plot.plot(res[0], res[1])
+res = sig.step(ss3)
+plot.plot(res[0], res[1])
+res = sig.step(ss31)
+plot.plot(res[0], res[1])
+
+plot.show()
+
+# Z jednej transmitancji możemy otrzymać różne równania stanu, jak w przypadku przykładu trzeciego gdzie widzimy gwałtowne wzrosty przy odpowiedzi skokowej
+# Natomiast konwersja z równań stanu do transmitancji jest zawsze tka sama
+# Trajektorie zmiennych stanu są różne w transmitancji, ponieważ przy konwersji na równania stanu możemy sobie dowolnie obrać co jest zmienną stanu
+# Warunki początkowe różnią się w obu reprezentacjach, przy transmitancji możemy jedynie zmieniać wielkość wymuszenia, ponieważ nie możemy "ustawić" stanu układu jak ma to miejsce w równaniach stanu
--- TS/2/2.py
+++ TS/2/2.py
@@ -1,20 +1,53 @@
 import numpy as np 
 import matplotlib.pyplot as plot
-from scipy.integrate import solve_ivp
 import scipy.signal as sig
-import math
 
-tf1 =  sig.TransferFunction([10], [1, 2])
-tf2 =  sig.TransferFunction([4], [2, 0, 1])
-tf3 =  sig.TransferFunction([-2, 6], [1, 7, 16, 12])
+tf1 =  sig.TransferFunction([1.0], [1.0, 1.0, 2.0])
+tf2 =  sig.TransferFunction([1.0], [1.0, -4.0, 2.0, 5.0])
+tf3 =  sig.TransferFunction([1.0], [1.0, 6.0, 4.5, -5.0, -6.0])
 
-ss1 = sig.StateSpace(-2, 10, 1, 0)
-ss2 = sig.StateSpace([[0, -1], [1, 0]], [[10],[0]], [1, 1], 0)
-res = sig.step(ss1)#sig.tf2ss(10, [1,2]))
+ss1 = sig.StateSpace(np.array([[-4.0, -1.0], [-2.0, -1.0]]), np.array([[2.0],[1.0]]), np.array([3.0, -4.0]), 0)
+ss2 = sig.StateSpace(np.array([[-1.0, 0.0, 1.0], [-6, 3, 5.0], [-5.0, -2.0, 4.0]]), np.array([[0], [1.0], [10.0]]), np.array([1.0, 1.0, 1.0]), 0)
+ss3 = sig.StateSpace(np.array([[-3.0,1.25, -0.75, -2.75], [-6.0, 3.0, -3.5, -6.0], [0.0, -1.0, 0.0, 1.0], [-6.0, 5.0, -4.5, -6.0]]), np.array([[0.5], [1.0], [0.0], [1.0]]), np.array([2.0, 0, 0, 0]), 0)
 
-ss11 = sig.tf2ss(10, [1,2])
-ss21 = sig.tf2ss(4, [2, 0, 1])
-ss31 = sig.tf2ss([-2, 6], [1, 7, 16, 12])
+ss11 = sig.ss2tf(ss1.A, ss1.B, ss1.C, ss1.D)
+ss21 = sig.ss2tf(ss2.A, ss2.B, ss2.C, ss2.D)
+ss31 = sig.ss2tf(ss3.A, ss3.B, ss3.C, ss3.D)
+
+t = np.linspace(0, 20, 100)
+u = np.sin(t)
+
+plot.figure()
+plot.title("System pierwszy")
+res = sig.step(tf1)
+res = sig.lsim(tf1, u, t)
 plot.plot(res[0], res[1])
+res = sig.step(ss1)
+res = sig.lsim(ss1, u, t)
+plot.plot(res[0], res[1])
+res = sig.step(ss11)
+res = sig.lsim(ss11, u, t)
+plot.plot(res[0], res[1])
+
+plot.figure()
+plot.title("System drugi")
+res = sig.step(tf2)
+plot.plot(res[0], res[1])
+res = sig.step(ss2)
+plot.plot(res[0], res[1])
+res = sig.step(ss21)
+plot.plot(res[0], res[1])
+
+plot.figure()
+plot.title("System trzeci")
+res = sig.step(tf3)
+plot.plot(res[0], res[1])
+res = sig.step(ss3)
+plot.plot(res[0], res[1])
+res = sig.step(ss31)
+plot.plot(res[0], res[1])
+
 plot.show()
 
+
+# Wyniki są drastycznie różne, możliwe jest otrzymanie różnychh wyników, transmitancja jest stosunkiem wejścia do wyjścia, zachhowując ten stosunek możemy otrzymać różne odpowiedzi
