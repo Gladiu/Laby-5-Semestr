95299df1610d662200e2c8a740092e4891f3502e
Gladiu <maurycyk1@tlen.pl>
2021-11-20 13:38:49 +0000

    modified:   6.py

--- USO/5/6.py
+++ USO/5/6.py
@@ -9,10 +9,9 @@ def add_transfer_functions(transfer_1, transfer_2):
     denominator = np.polymul(transfer_1.den, transfer_2.den)
     return sig.TransferFunction(nominator, denominator)
 
-def find_nearest(array, value):
-    array = np.asarray(array)
+def find_nearest_index(array, value):
     idx = (np.abs(array - value)).argmin()
-    return array[idx]
+    return idx
 
 def main():
     # Parametry układu
@@ -20,29 +19,31 @@ def main():
     C = 0.5
     R = 10
 
-    # Parametry regulatora PID
-    Kp = 2
-    Ti = 1
-    Td = 0.4
-
     simulated_object = sig.TransferFunction([1],[L, R, 1/C])
+    
+    plot.figure()
+    plot.title("Step response of an object")
+    res = sig.step(simulated_object)
+    plot.plot(res[0], res[1])
 
-    # Parameters of regulator
+    # Parametry regulatora PID
+    T0 = res[0][find_nearest_index(res[1], np.amax(res[1])*(0.2))]
+    T = res[0][find_nearest_index(res[1], np.amax(res[1])*(0.8))] - T0
+    K = 1
+
+    Kp = 1.2*T/K*T0
+    Ti = 2*T0
+    Td = 0.4*T0
     regulator = sig.TransferFunction([Td*Ti*Kp, Ti*Kp, Kp], [Ti, 0])
-
     open_loop = sig.TransferFunction(np.polymul(regulator.num, simulated_object.num),  np.polymul(regulator.den, simulated_object.den))
     closed_loop = sig.TransferFunction([open_loop.den], np.polyadd(open_loop.den, open_loop.num))
 
-    
     plot.figure()
-    res = sig.step(simulated_object)
+    plot.title("Step response of an object with PID regulator tuned with Ziegler Nichols")
+    res = sig.step(closed_loop)
     plot.plot(res[0], res[1])
     plot.show()
-    steady_state_index = np.where(res[1] == np.amax(res[1])
-    steady_state_index = np.where(res[1] == np.amax(res[1])
-        T =     
-    T0 =     
-    K =     
+
 
 if __name__=='__main__':
     main()
