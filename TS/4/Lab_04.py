e75c964b8499bde9181384edb241c6475eb075aa
Gladiu <maurycyk1@tlen.pl>
2021-11-28 23:19:53 +0100

    modified:   Lab_04.py

--- TS/4/Lab_04.py
+++ TS/4/Lab_04.py
@@ -7,6 +7,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
+
+
+Zadanie_2 = False
+Zadanie_3 = True
+Zadanie_4 = False
+Zadanie_5 = False
 #initialize constant values
 b = 0.5
 k = 1
@@ -19,10 +25,16 @@ samples = int(Tf/Tp + 1)
 
 T = np.linspace(0,Tf,samples)
 
-#prepare all input signals - control U and noised W and V
-U = np.full(samples, 1)
-W = np.random.normal(0,1,samples)
-V = np.array([np.random.normal(0,0,samples),np.random.normal(0,0,samples)]) #for now assume no process noise V (model is perfect)
+if Zadanie_2 or Zadanie_3:
+    #prepare all input signals - control U and noised W and V
+    U = np.full(samples, 1)
+    W = np.random.normal(0,1,samples)
+    V = np.array([np.random.normal(0,0,samples),np.random.normal(0,0,samples)]) #for now assume no process noise V (model is perfect)
+if Zadanie_4:
+    #prepare all input signals - control U and noised W and V
+    U = np.full(samples, 1)
+    W = np.random.normal(0,1,samples)
+    V = np.array([np.random.normal(0,0,samples),np.random.normal(0,0,samples)]) #for now assume no process noise V (model is perfect)
 
 #define arrays A, B and C and D of a linear system
 A = np.array([[0, 1],
@@ -75,38 +87,59 @@ plt.grid()
 A = (A*Tp + np.eye(2))
 B = B*Tp
 
-#initialize all matrices of Kalman Filter - feel free to change these values  
-Xc = np.array([[0],[0]]) #Xc (a posteriori estimate X) - assumed initial conditions
-Pc = np.array([[0,0],[0,0]]) #Pc (a posteriori P) - how unsure we are of initial conditions
-#a priori estiamtes Xp and Pp are not initialized, as they will be calculated based on a posteriori values and measured signals
+if Zadanie_2:
+    #initialize all matrices of Kalman Filter - feel free to change these values  
+    Xc = np.array([[0],[0]]) #Xc (a posteriori estimate X) - assumed initial conditions
+    Pc = np.array([[0,0],[0,0]]) #Pc (a posteriori P) - how unsure we are of initial conditions
+    #a priori estiamtes Xp and Pp are not initialized, as they will be calculated based on a posteriori values and measured signals
 
-Q = np.array([[0,0],[0,0]]) #Q - assumed covariance of process noise
-R = np.cov(W) #R - assumed covariance of measurement noise
+    Q = np.array([[0,0],[0,0]]) #Q - assumed covariance of process noise
+    R = np.cov(W) #R - assumed covariance of measurement noise
+if Zadanie_3:
+    #initialize all matrices of Kalman Filter - feel free to change these values  
+    Xc = np.array([[3],[-1]]) #Xc (a posteriori estimate X) - assumed initial conditions
+    Pc = np.array([[3, 0],[0,-1]]) #Pc (a posteriori P) - how unsure we are of initial conditions
+    #a priori estiamtes Xp and Pp are not initialized, as they will be calculated based on a posteriori values and measured signals
+
+    Q = np.array([[2.5,0.125],[.5, 0.125]]) * 0.1 #Q - assumed covariance of process noise
+    R = 100 #R - assumed covariance of measurement noise
 
 #some vectors to keep samples of Xc aposteriori and Xp apriori estimate for later plotting
 XC = Xc.T
 XP = Xc.T
 
-#main loop of discrete-time simulation
-for i in range(0,samples - 1):  
-    # Kalman filter
-    
-    #
-    # TODO - implement Kalman Filter with Xc as sample a priori estimate and Xp as sample a posteriori estimate
-    #
-    # A priori
-    Xp = A @ XC[i] + B @ U
-    Pp = A @ PC[i] @ A.T + Q 
-    # A posteriori
-    Xc = Xp + K_array[i] @ e_array[i]
-    e_array[i] = Y[i] - C @ Xp
-    Pc = 
-    S = C @ Pp @ C.T + R[i]
-    PP = PC[i] - K
 
-    # Add calculated values to XC and XP 
-    XC = np.vstack([XC, Xc.T])
-    XP = np.vstack([XP, Xp.T])
+#main loop of discrete-time simulation
+for n in range(0,samples - 1):  
+    # Kalman filter
+    if Zadanie_2 or Zadanie_3:
+        # A priori
+        Xp = A @ Xc + B * U[n]
+        Pp = A @ Pc @ A.T + Q 
+        # A posteriori
+        e = Y[n] - C @ Xp
+        S = C @ Pp @ C.T + R
+        K = Pp @ C.T @ np.linalg.inv(S)
+        Pc = Pp - K @ S @ K.T
+        Xc = Xp + K @ e
+
+        # Add calculated values to XC and XP 
+        XC = np.vstack([XC, Xc.T])
+        XP = np.vstack([XP, Xp.T])
+    if Zadanie_4:
+        # A priori
+        Xp = A @ Xc + B * U[n]
+        Pp = A @ Pc @ A.T + Q 
+        # A posteriori
+        e = Y[n] - C @ Xp
+        S = C @ Pp @ C.T + R
+        K = Pp @ C.T @ np.linalg.inv(S)
+        Pc = Pp - K @ S @ K.T
+        Xc = Xp + K @ e
+
+        # Add calculated values to XC and XP 
+        XC = np.vstack([XC, Xc.T])
+        XP = np.vstack([XP, Xp.T])
 
 
 #plot the results
@@ -124,3 +157,5 @@ plt.figure()
 plt.plot(T, X - XC)
 plt.title("Estimation errors")
 plt.grid()
+
+plt.show()
