e7ea6f10456cfeefe969d4594223cba4be86cb6d
Gladiu <maurycyk1@tlen.pl>
2021-12-01 19:57:04 +0100

    Poddaje sie

--- TS/5/1.py
+++ TS/5/1.py
@@ -49,11 +49,26 @@ for i in range(len(V0)):
 pyplot.show()
 
 # Zadanie 4
-# e' = A*e-B*u-A*x_d
-# e' = A*e-B*(Vd/Vin+ufb)-A*x_d
-# e' = A*e-B*(Vd/Vin)-B*u_fb-A*x_d
-# e' = A*e-B*u_fb-A*x_d-B*(Vd/Vin)
-# e' = A*e-B*u_fb-[[0], [Vd/-C*L]]-B*(Vd/Vin)
+# e' = A*e - B*u - A*x_d
+# e' = A*e - B*(Vd/Vin+ufb) - A*x_d
+# e' = A*e - B*(Vd/Vin) - B*u_fb - A*x_d
+# e' = A*e - B*u_fb - A*x_d - B*(Vd/Vin)
+# e' = A*e - B*u_fb - [[0], [Vd/-C*L]] - B*(Vd/Vin)
 # Podstawiamy B = np.array([[0],[Vin/C*L]])
-# e' = A*e-B*u_fb-[[0], [Vd/-C*L]]-[[0],[Vd/C*L]]
-# e' = A*e-B*u_fb
+# e' = A*e - B*u_fb - [[0], [Vd/-C*L]] - [[0],[Vd/C*L]]
+# e' = A*e - B*u_fb
+
+# Zadanie 5
+# e' = Ae-B*K*e
+# e' = (A-B*K)*e
+k1 = sym.Symbol('k1')
+k2 = sym.Symbol('k2')
+K = np.array([[k1, k2]])
+
+eigen_values = sym.Matrix((A-B@K) - l*np.eye(2)).det()
+#eigen_values[0].sub(l, -5)
+desired_eigen_values = [-5, -1, -10]
+#for i in range(len(desired_eigen_values)):
+i = 0
+#print(sym.solve(sym.Eq(desired_eigen_values[i], eigen_values[0]), [k1, k2]))
+print(eigen_values.subs(l, -5))
