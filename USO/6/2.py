111aad8b8c752309c89f2b331189cc350f521feb
gladiu <maurcyk1@tlen.pl>
2021-12-02 14:55:51 +0100

    modified:   2.py

--- USO/6/2.py
+++ USO/6/2.py
@@ -19,23 +19,49 @@ R = np.eye(1)
 P = linalg.solve_continuous_are(A, B, Q, R)
 
 # 2.4
+#K = np.linalg.inv(R) @ B.T @ P
 K = R @ B.T @ P
-
 # 2.2
 j = []
 def model(x,t):
     x = np.array(x)
     u = -K @ x
     x_next = A@np.array([[x[0]],[x[1]]]) + B @ u
-    j.append(x.T @ Q @ x + u.T @ R @ u)
+    j.append(x.T @ Q @ x+ u.T @ R @ u)
     return [x_next[0][0],x_next[1][0]]
 
 t = np.linspace(0, 5, 100)
 res = integ.odeint(model, [1, 0], t)
 plot.plot(t, res[:, 0])
+plot.plot(t, res[:, 1])
+for i in range(len(j)):
+    if i >0:
+        j[i] = j[i-1] + j[i]
 plot.plot(np.linspace(0, 5, len(j)), j)
 
+# 2.7
+plot.figure()
 
+j = []
+qd = 5.0
+xd = np.array([[qd], [0]])
+uarray = []
+def model_modded(x,t):
+    x = np.array(x)
+    e = xd - np.array([[x[0]],[x[1]]])
+    ue = -K @ e
+    u = -ue + (qd/(C)) # W instrukcji we wzorze numer 10 jest nieścisłość jest napisane 1/C
+    x_next = A@np.array([[x[0]],[x[1]]]) + B @ u
+    j.append(e.T @ Q @ e + ue.T @ R @ ue)
+    return [x_next[0][0],x_next[1][0]]
 
-
+t = np.linspace(0, 5, 100)
+res = integ.odeint(model_modded, [0, 0], t)
+plot.plot(t, res[:, 0])
+plot.plot(t, res[:, 1])
+#plot.plot(np.linspace(0, 5, len(j)), j)
+for i in range(len(j)):
+    if i >0:
+        j[i] = j[i-1] + j[i]
+print(j)
 plot.show()
