99e64cd2fd161553d40b3667f1a3e2046654dc98
gladiu <maurcyk1@tlen.pl>
2021-12-02 12:33:59 +0100

    Zadanie 10

---
+++ WDSI/6/10_cont_linear_world_speed_cln/agent.py
@@ -0,0 +1,73 @@
+# prob.py
+# This is
+
+import random
+import numpy as np
+import queue
+import math
+
+from gridutil import *
+
+
+class Agent:
+    def __init__(self, size, walls, loc, dir, sigma_move, sigma_perc, dt):
+        self.size = size
+        self.walls = walls
+        # list of valid locations
+        self.locations = list({*locations(self.size)}.difference(self.walls))
+        # dictionary from location to its index in the list
+        self.loc_to_idx = {loc: idx for idx, loc in enumerate(self.locations)}
+        self.loc = loc
+        self.dir = dir
+        self.dt = dt
+        self.action_dir = 1
+
+        self.sigma_move = sigma_move
+
+        self.t = 0
+
+        # create matrices used in Kalman filter
+        # TODO PUT YOUR CODE HERE
+        self.mu = np.array([[0],[0]])
+        self.Sigma = np.array([[1, 0],[0, 10]])
+
+
+        # ------------------
+
+    def __call__(self):
+        # use information about requested action to update posterior
+        # TODO PUT YOUR CODE HERE
+        self.predict_posterior()
+
+        # ------------------
+
+        # this function does not return anything
+        return
+
+    def predict_posterior(self):
+        # predict posterior
+        # TODO PUT YOUR CODE HERE
+        Q = np.array([[0.25*self.dt ** 4, 0.5 * self.dt ** 3], [0.5 * self.dt ** 3, self.dt ** 2]]) * self.sigma_move**2
+
+        F = np.array([[1, self.dt], [0, 1]])
+        
+        self.mu = F @ self.mu
+        self.Sigma = F @ self.Sigma @ F.T + Q
+
+        # ------------------
+
+        # this function does not return anything
+        return
+
+    def correct_posterior(self, percept):
+        # correct posterior using measurements
+        # TODO PUT YOUR CODE HERE
+
+
+        # ------------------
+
+        # this function does not return anything
+        return
+
+    def get_posterior(self):
+        return self.mu, self.Sigma
