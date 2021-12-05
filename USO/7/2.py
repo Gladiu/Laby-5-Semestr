b3d2923f90fc8679b7313c66fdf0a65f126b1771
Gladiu <maurycyk1@tlen.pl>
2021-12-06 00:10:07 +0100

    new file:   2.py

---
+++ USO/7/2.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy.interpolate as interp
+import scipy.integrate as integ
+import matplotlib.pyplot as plot 
+
+R = 0.5
+C = 0.5
+L = 0.2
+
+A = np.array([[0, 1],[-1/(L*C), -R/L]])
+B = np.array([[0], [1/L]])
+R = np.eye(1)
+Q = np.eye(2)
+
+# 2.1
+def riccati(p,t):
+    print(t)
+    p = np.array([[p[0], p[1]],[p[2], p[3]]])
+    next_p = p @ A - p @ B @ np.linalg.inv(R) + A.T @ p + Q
+    return [next_p[0][0], next_p[0][1], next_p[1][0], next_p[1][0]]
+
+
+# 2.2
+t = np.linspace(0, 5, 100)
+P = integ.odeint(riccati, [0,0,0,0], t)
+plot.figure()
+plot.title("Wartości macierzy P w czasie")
+plot.subplot(2,2,1)
+plot.plot(t, P[:, 0])
+plot.subplot(2,2,2)
+plot.plot(t, P[:, 1])
+plot.subplot(2,2,3)
+plot.plot(t, P[:, 2])
+plot.subplot(2,2,4)
+plot.plot(t, P[:, 3])
+
+# 2.3
+
+P_0 = interp.interp1d(t, P[:,0])
+P_1 = interp.interp1d(t, P[:,1])
+P_2 = interp.interp1d(t, P[:,2])
+P_3 = interp.interp1d(t, P[:,3])
+
+t = np.linspace(0, 5, 100)
+def model(x,t):
+    x = np.array([[x[0]], [x[1]]])
+    current_p = np.array([[P_0(t), P_1(t)],[P_2(t), P_3(t)]])
+    K = np.linalg.inv(R) @ B.T @ current_p
+    u = -K @ x
+    x_next = A @ x + B @ u
+    return [x_next[0][0], x_next[1][0]]
+
+res = integ.odeint(model, [1, 1], t)
+
+plot.show()
