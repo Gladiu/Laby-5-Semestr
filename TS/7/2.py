74890af04b749b25221b431fd8864c40b89999b4
gladiu <maurcyk1@tlen.pl>
2022-01-13 11:12:21 +0100

    modified:   2.py

--- TS/7/2.py
+++ TS/7/2.py
@@ -50,10 +50,78 @@ for i in range(precision):
 #V = 0.5 * x.T @ P @ x
 #V_prime = -0.5 * x.T @ Q @ x
 
+Vd = 5
+def model(x,t):
+    x_next = A @ np.array([[x[0]], [x[1]]]) + B * (Vd/Vin + K @(np.array([[Vd],[0]]) - np.array([[x[0]], [x[1]]])))
+    return [x_next[0][0], x_next[1][0]]
+
+t = np.linspace(0,5,100)
+res = odeint(model, [0, 0], t)
+plot.figure()
+
+plot.plot(t, res[:,0])
+plot.plot(t, res[:,1])
+V_sim = []
+for x1 in res[:,0]:
+    for x2 in res[:,1]:
+        x_temp = np.array([[x1],[x2]])
+        temp_V = x_temp.T @ P @ x_temp
+        temp_V *= 0.5
+        V_sim.append(temp_V)
+#ax = add_subplot(projection='3d')
 plot.figure()
 
 ax = plot.axes(projection='3d')
 ax.plot_surface(xmesh ,ymesh ,V, cmap='viridis', edgecolor='none')
 ax.plot_surface(xmesh ,ymesh ,V_prime, cmap='viridis', edgecolor='none')
+ax = plot.axes(projection='3d')
+ax = fig.add_subplot(projection='3d')
+ax.plot(res[:,0], res[:,1], V_sim, cmap='viridis', edgecolor='none')
+
+# 2.4
+k1 = -499/4000
+k2 = -9/4000
+
+K = np.array([[k1,k2]])
+H = (A-B@K)
+H = np.array(H)
+Q = np.array([[1, 0],[0,1]])
+
+P = solve_continuous_lyapunov(H.T,-Q)
+precision = 100
+x = np.linspace(-10,10,precision)
+x = np.vstack((x,x))
+xmesh, ymesh = np.meshgrid( np.linspace(-10,10,precision), np.linspace(-10,10,precision))
+V = np.zeros(shape = xmesh.shape)
+V_prime = np.zeros(shape = xmesh.shape)
+for i in range(precision):
+    for j in range(precision):
+        x_temp = np.array([[xmesh[i][j]],[ymesh[i][j]]])
+        V[i][j] = x_temp.T @ P @ x_temp
+        V[i][j] *= 0.5 
+        V_prime[i][j] = x_temp.T @ Q @ x_temp
+        V_prime[i][j] *= -0.5 
+
+#V = 0.5 * x.T @ P @ x
+#V_prime = -0.5 * x.T @ Q @ x
+
+plot.figure()
+
+ax = plot.axes(projection='3d')
+ax.plot_surface(xmesh ,ymesh ,V, cmap='viridis', edgecolor='none')
+ax.plot_surface(xmesh ,ymesh ,V_prime, cmap='viridis', edgecolor='none')
+
+Vd = 5
+def model(x,t):
+    x_next = A @ np.array([[x[0]], [x[1]]]) + B * (Vd/Vin + K @(np.array([[Vd],[0]]) - np.array([[x[0]], [x[1]]])))
+    return [x_next[0][0], x_next[1][0]]
+
+t = np.linspace(0,5,100)
+res = odeint(model, [0, 0], t)
+plot.figure()
+
+plot.plot(t, res[:,0])
+plot.plot(t, res[:,1])
+
 plot.show()
 
