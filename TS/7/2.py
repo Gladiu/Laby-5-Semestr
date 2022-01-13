d863659c290c78b8a4c61c70796ba17a86496a3a
gladiu <maurcyk1@tlen.pl>
2022-01-13 10:47:44 +0100

    modified:   2.py

--- TS/7/2.py
+++ TS/7/2.py
@@ -9,8 +9,7 @@ L = 0.1
 Vin = 8
 R = 5
 Capacitor = 0.01
-
-A = np.array([[0, 1],[-1/(L*Capacitor),-1/(L*R)]])
+A = np.array([[0, 1],[-1/(L*Capacitor),-1/(Capacitor*R)]])
 B = np.array([[0],[Vin/(Capacitor*L)]])
 C = np.array([1, 0])
 
@@ -21,32 +20,40 @@ k2 = sym.Symbol('k2')
 # 2.1
 K = np.array([[k1,k2]])
 eigen_values = sym.Matrix(l*np.eye(2) - (A-B@K)).det()
-#print(eigen_values)
+print(eigen_values)
 
-k1 = (-999/8000)
-k2 = 0 
+k1 = (-999.0/8000.0)
+k2 = -9/4000
 
 # 2.2
 
 K = np.array([[k1,k2]])
-H = A-B@K
+H = (A-B@K)
+H = np.array(H)
+Q = np.array([[1, 0],[0,1]])
 
-Q = np.array([[1, 1],[1,1]])
-P = solve_continuous_lyapunov(H,Q)
-
-x = np.linspace(-10,10,1000)
+P = solve_continuous_lyapunov(H.T,-Q)
+precision = 100
+x = np.linspace(-10,10,precision)
 x = np.vstack((x,x))
+xmesh, ymesh = np.meshgrid( np.linspace(-10,10,precision), np.linspace(-10,10,precision))
+V = np.zeros(shape = xmesh.shape)
+V_prime = np.zeros(shape = xmesh.shape)
+for i in range(precision):
+    for j in range(precision):
+        x_temp = np.array([[xmesh[i][j]],[ymesh[i][j]]])
+        V[i][j] = x_temp.T @ P @ x_temp
+        V[i][j] *= 0.5 
+        V_prime[i][j] = x_temp.T @ Q @ x_temp
+        V_prime[i][j] *= -0.5 
 
-V = 0.5 * x.T @ P @ x
-V_prime = -0.5 * x.T @ Q @ x
+#V = 0.5 * x.T @ P @ x
+#V_prime = -0.5 * x.T @ Q @ x
 
 plot.figure()
 
 ax = plot.axes(projection='3d')
-#ax.plot_surface(x,x,V, cmap='viridis', edgecolor='none')
-xmesh, ymesh = np.meshgrid( np.linspace(-10,10,1000), np.linspace(-10,10,1000))
-#ax.plot_surface(xmesh ,ymesh ,V, cmap='viridis', edgecolor='none')
+ax.plot_surface(xmesh ,ymesh ,V, cmap='viridis', edgecolor='none')
 ax.plot_surface(xmesh ,ymesh ,V_prime, cmap='viridis', edgecolor='none')
-
 plot.show()
 
